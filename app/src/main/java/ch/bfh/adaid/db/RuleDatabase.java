762563b2170c8ca66f5b1c45147ea739b775258e
NikLeberg <niklaus.leuenb+github@gmail.com>
2022-09-12 20:43:31 +0200

    db: extend Rule entity with relativePath column

--- app/src/main/java/ch/bfh/adaid/db/RuleDatabase.java
+++ app/src/main/java/ch/bfh/adaid/db/RuleDatabase.java
@@ -2,22 +2,27 @@ package ch.bfh.adaid.db;
 
 import android.content.Context;
 
+import androidx.room.AutoMigration;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 /**
- * Room database. Class is a singleton as each database instance has a heavy cost on resources.
+ * Room database. Class is a singleton as each database instance has a heavy
+ * cost on resources.
  * <p>
  * Source: https://developer.android.com/codelabs/android-room-with-a-view#7
  *
  * @author Niklaus Leuenberger
  */
-@Database(entities = {Rule.class}, version = 1)
+@Database(entities = { Rule.class }, version = 2, autoMigrations = {
+        @AutoMigration(from = 1, to = 2)
+})
 public abstract class RuleDatabase extends RoomDatabase {
 
     /**
-     * RuleDao getter. Is abstract but gets automatically implemented by androidx.room.
+     * RuleDao getter. Is abstract but gets automatically implemented by
+     * androidx.room.
      *
      * @return Rule database access object.
      */
@@ -36,7 +41,6 @@ public abstract class RuleDatabase extends RoomDatabase {
             synchronized (RuleDatabase.class) {
                 if (INSTANCE == null) {
                     // Instantiate / build the database if no instance exists yet.
-                    // TODO: Add migration logic. (https://medium.com/androiddevelopers/understanding-migrations-with-room-f01e04b07929)
                     INSTANCE = Room.databaseBuilder(context, RuleDatabase.class, "rule_database")
                             .build();
                 }
